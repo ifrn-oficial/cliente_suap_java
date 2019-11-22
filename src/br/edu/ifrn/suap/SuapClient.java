2a8cc20f27a9c516d5f810b51aa5fc2bdd336c65
Breno Silva <breno.silva@ifrn.edu.br>
2019-11-22 11:49:31 -0300

    Primeiro commit

---
+++ src/br/edu/ifrn/suap/SuapClient.java
@@ -0,0 +1,81 @@
+package br.edu.ifrn.suap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+public class SuapClient {
+	
+	private static final String AUTHORIZE_URL= "https://suap.ifrn.edu.br/o/authorize/";
+	private static final String TOKEN_URL= "https://suap.ifrn.edu.br/o/token/";
+	private static final String REDIRECT_URL= "http://localhost:8000";
+	
+	private String id;
+	private String secret;
+
+	public SuapClient(String id, String secret){
+		this.id = id;
+		this.secret = secret;
+	}
+
+	public String authorize() throws IOException{
+
+		ServerSocket server = new ServerSocket(8000);
+		String url = AUTHORIZE_URL+"?response_type=code&grant_type=authorization-code=&client_id="+this.id;
+		Runtime.getRuntime().exec("sensible-browser "+url);
+		
+		while (true) {
+			try (Socket socket = server.accept()) {
+				
+				InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+				BufferedReader reader = new BufferedReader(isr);
+				String line = reader.readLine();
+				String code = null;
+				String token = null;
+				String httpResponse;
+				while (!line.isEmpty()) {
+					if(line.indexOf("GET /?code=")==0){
+						code = line.split("=")[1].split(" ")[0];
+						break;
+					}
+					line = reader.readLine();
+				}
+				if(code!=null){
+					String params = "client_id="+this.id+"&client_secret="+this.secret+"&code="+code+"&redirect_uri="+REDIRECT_URL+"&grant_type=authorization_code";
+					HttpClient client = HttpClient.newHttpClient();
+					HttpRequest request = HttpRequest.newBuilder().uri(URI.create(TOKEN_URL)).header("Content-Type", "application/x-www-form-urlencoded").POST(HttpRequest.BodyPublishers.ofString(params)).build();
+					HttpResponse<String> response = client.send(request,
+					HttpResponse.BodyHandlers.ofString());
+					token = response.body();
+					httpResponse = "HTTP/1.1 200 OK\r\n\r\nSuccess!!!";
+
+				} else {
+					httpResponse = "HTTP/1.1 200 OK\r\n\r\nFailed!!!";
+				
+				}
+				socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+				socket.getOutputStream().flush();
+				socket.getOutputStream().close();
+				return token;
+			} catch (Exception e) {
+				e.printStackTrace();
+				server.close();
+				return null;
+			}
+		}
+	}
+
+	public static void main(String args[]) throws IOException {
+		SuapClient client = new SuapClient("XXXXXXX", "XXXXXXX");
+		String token = client.authorize();
+		System.out.println(token);
+	}
+
+}
